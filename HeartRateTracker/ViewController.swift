ce4a619b968a0dc57ae49a0aa83a6c669f234f6f
Federico <Serzherio@yandex.ru>
2022-05-11 10:56:56 +0300

    Initial Commit

---
+++ HeartRateTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeartRateTracker
+//
+//  Created by Сергей on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
